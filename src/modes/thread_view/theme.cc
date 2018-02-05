f83a92f4a74fe16847172d2fc78345ead8818ef7
Robin Jarry <robin.jarry@6wind.com>
2018-02-05 16:36:48 +0100

    add sass function to access astroid config
    Signed-off-by: Robin Jarry <robin.jarry@6wind.com>

--- src/modes/thread_view/theme.cc
+++ src/modes/thread_view/theme.cc
@@ -10,6 +10,8 @@
 # include "utils/resource.hh"
 
 # ifndef DISABLE_LIBSASS
+# include <boost/regex.hpp>
+# include "csscolorparser.hpp"
 
 # ifdef SASSCTX_CONTEXT_H
   # include <sass/context.h>
@@ -22,6 +24,7 @@
 
 using namespace std;
 using namespace boost::filesystem;
+using namespace boost::property_tree;
 
 namespace Astroid {
   std::atomic<bool> Theme::theme_loaded (false);
@@ -93,6 +96,64 @@ namespace Astroid {
   }
 
 # ifndef DISABLE_LIBSASS
+  static union Sass_Value *config_cb(const union Sass_Value *args,
+                                     Sass_Function_Entry cb,
+                                     struct Sass_Compiler* compiler) {
+    (void) cb;
+    (void) compiler;
+
+    LOG (debug) << "theme: astroid-config: called";
+
+    if (!(sass_value_is_list(args) && sass_list_get_length(args) == 2)) {
+      LOG (error) << "theme: astroid-config: cannot parse arguments";
+      return sass_make_null();
+    }
+
+    auto *conf = sass_list_get_value(args, 0);
+    auto *def_value = sass_list_get_value(args, 1);
+    auto *c = sass_string_get_value(sass_value_stringify(conf, false, 1));
+    ptree node;
+
+    try {
+      node = astroid->config().get_child(c);
+    } catch (ptree_bad_path) {
+      return def_value;
+    }
+
+    try {
+      // try to convert to boolean
+      bool value = node.get_value<bool>();
+      return sass_make_boolean(value);
+    } catch (ptree_bad_data) {
+      // pass
+    }
+
+    string s_value;
+
+    try {
+      s_value = node.get_value<string>();
+    } catch (ptree_bad_data) {
+      return def_value;
+    }
+
+    // try to convert to number + unit
+    static boost::regex exp("\\s*(\\d+(?:\\.\\d+))([a-z%]*)\\s*");
+    boost::smatch match;
+    if (boost::regex_match(s_value, match, exp, boost::match_extra)) {
+      double number = boost::lexical_cast<double>(match[1]);
+      return sass_make_number(number, match[2].str().c_str());
+    }
+
+    // try to convert to css color
+    boost::optional<CSSColorParser::Color> color;
+    color = CSSColorParser::parse(s_value);
+    if (color) {
+      return sass_make_color(color->r, color->g, color->b, color->a);
+    }
+
+    return sass_make_string(s_value.c_str());
+  }
+
   ustring Theme::process_scss (const char * scsspath) {
     /* - https://github.com/sass/libsass/blob/master/docs/api-doc.md
      * - https://github.com/sass/libsass/blob/master/docs/api-context-example.md
@@ -106,6 +167,12 @@ namespace Astroid {
     struct Sass_Context* context = sass_file_context_get_context(file_ctx);
     sass_option_set_precision(options, 1);
     sass_option_set_source_comments(options, true);
+    // allocate a custom function caller
+    Sass_Function_Entry fn_config = sass_make_function(
+            "astroid-config($conf, $default: '')", config_cb, NULL);
+    Sass_Function_List fn_list = sass_make_function_list(1);
+    sass_function_set_list_entry(fn_list, 0, fn_config);
+    sass_option_set_c_functions(options, fn_list);
 
     int status = sass_compile_file_context (file_ctx);
 
